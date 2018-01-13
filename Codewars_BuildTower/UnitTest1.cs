aebb28107ec711a3ca81094fdc9609c0b525de18
sqz777In91 <sqz777isme@gmail.com>
2018-01-13 15:43:16 +0800

    fix test case and production code

--- Codewars_BuildTower/UnitTest1.cs
+++ Codewars_BuildTower/UnitTest1.cs
@@ -8,15 +8,15 @@ namespace Codewars_BuildTower
         [TestMethod]
         public void PrintStar_Input_1_Should_Be_1Star()
         {
-            Assert.AreEqual("*", Kata.PrintStar(1));
-        }
+            Assert.AreEqual(new string[] { "*" }, Kata.PrintStar(1));
     }
+}
 
-    public class Kata
+public class Kata
+{
+    public static string[] PrintStar(int n)
     {
-        public static string PrintStar(int n)
-        {
-            throw new System.NotImplementedException();
-        }
+        throw new System.NotImplementedException();
     }
+}
 }
